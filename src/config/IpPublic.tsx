c2929524c76e471eeea84a55f7045cb9a335a053
chikakhanza <chikfebria26@gmail.com>
2025-12-24 22:24:34 +0700

    chika

--- src/config/IpPublic.tsx
+++ src/config/IpPublic.tsx
@@ -1,2 +1,2 @@
-export const API_URL = `http://192.168.1.40:8000/api`;
-export const IP_PUBLIC = `http://192.168.1.40:8000`;
+export const API_URL = `https://dina-unmelodised-ferociously.ngrok-free.dev/api`;
+export const IP_PUBLIC = `https://dina-unmelodised-ferociously.ngrok-free.dev`;
